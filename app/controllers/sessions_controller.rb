4dd816f0e4b0cd3348ffae52da7e3f5bbe0852d3
App Academy Student <student@appacademy.io>
2014-09-23 11:25:35 -0700

    session controller implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+  def new
+    
+  end
+  
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:email],
+      params[:user][:password])
+    if @user
+      sign_in!(user)
+      redirect_to root_url
+    else
+      flash.now[:errors] = ["Invalid email and/or password"]
+      render :new
+    end
+  end
+  
+  def destroy
+    sign_out!
+    redirect_to new_session_url
+  end
 end
