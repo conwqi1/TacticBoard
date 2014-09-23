813a626ed4868895333e53d7a6dfa77fed299e2c
App Academy Student <student@appacademy.io>
2014-09-23 11:11:21 -0700

    application controller implemented

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,2 +1,6 @@
 class Board < ActiveRecord::Base
+  validates :title, :user, presence: true
+  belongs_to :user
+  #association lists are deleted when owner board is deleted 
+  has_many :lists, dependent: :destroy
 end
