60a101888a6fe93bd857aec4f76f488ef9ba5863
App Academy Student <student@appacademy.io>
2014-09-24 12:00:58 -0700

    add boards implemented

--- app/assets/javascripts/views/boards/boards_index.js
+++ app/assets/javascripts/views/boards/boards_index.js
@@ -6,7 +6,7 @@ TrelloVideo.Views.BoardsIndex = Backbone.View.extend({
   className: 'boards-index',
   
   initialize: function(){
-    this.listenTo(this.collection, 'sync', this.render);
+    this.listenTo(this.collection, 'sync add destroy', this.render);
   },
   
   
@@ -17,5 +17,30 @@ TrelloVideo.Views.BoardsIndex = Backbone.View.extend({
     this.$el.html(renderContent);
     return this;
   },
+  
+  events: {
+    "submit #createBoard": "addBoard"
+  },
+  
+  addBoard: function(event){
+    event.preventDefault();
+    var $target = $(event.currentTarget); 
+    var title = $target.find('#board_title').val();
+    var model = new TrelloVideo.Models.Board({title: title});
+    this.collection.create(model)
+    Backbone.history.navigate("", { trigger: true });
+  },
+  
+  events: {"click #delete_board":"deleteBoard"},
+  
+  deleteBoard: function(event){
+    event.preventDefault();
+    var $target = $(event.currentTarget); 
+    var boardId = $target.data('id');
+    this.model = this.collection.get('boardId');
+    this.model.destroy();
+  },    
+   
+
   
 });
