b272e1baac16de5ba8cc88a7228ed61087244c6b
App Academy Student <student@appacademy.io>
2014-09-27 12:04:13 -0700

    fixed problem when adding list

--- app/assets/javascripts/views/boards/board_show.js
+++ app/assets/javascripts/views/boards/board_show.js
@@ -17,7 +17,7 @@ TrelloVideo.Views.BoardShow = Backbone.CompositeView.extend({
     var $target = $(event.currentTarget);
     var title = $target.find('#list_title').val();
     var boardId = $target.data('id');
-    var ord = this.collection.length();
+    var ord = this.collection.length;
     this.collection.create({
       board_id: boardId,
       title: title,
